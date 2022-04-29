74e92d57d66c9c1e6f3ef7a867c7995bdf3cf26a
Ainsley Brundage <abrundage95@gmail.com>
2022-04-29 00:44:15 -0400

    Completed project 7

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [width, setWidth] = useState('0%');
+
+  function Game() {
+    const openNav = () => {
+      setWidth('20%');
+    };
+    return (
+      <div id="icon" onClick={openNav}>
+        🎮
+      </div>
+    );
+  }
+
+  function GameChanger() {
+    return <div class="container"></div>;
+  }
+
+  function List() {
+    const closeNav = () => {
+      setWidth('0px');
+    };
+
+    return (
+      <div id="mySidenav" class="sidenav" style={{ width: width }}>
+        <a href="javascript:void(0)" class="closebtn" onClick={closeNav}>
+          &times;
+        </a>
+        <a
+          class="fox"
+          href="http://www.chinatownfair.biz/"
+          onClick={GameChanger}
+        >
+          Chinatown Fair
+        </a>
+        <a class="fox" href="https://www.os-nyc.com/" onClick={GameChanger}>
+          OS NYC
+        </a>
+        <a class="fox" href="https://acebar.com/" onClick={GameChanger}>
+          Ace Bar
+        </a>
+        <a class="fox" href="https://barcadestmarks.com/" onClick={GameChanger}>
+          Barcade (St. Marks)
+        </a>
+        <a class="fox" href="https://barcadenewyork.com/" onClick={GameChanger}>
+          Barcade (24th St)
+        </a>
+        <a class="fox" href="https://tidalforcevr.com/" onClick={GameChanger}>
+          Tidal Force VR
+        </a>
+        <a
+          class="fox"
+          href="https://www.daveandbusters.com/"
+          onClick={GameChanger}
+        >
+          Dave and Buster's
+        </a>
+        <a
+          class="fox"
+          href="https://www.bowlero.com/location/bowlero-times-square"
+          onClick={GameChanger}
+        >
+          Bowlero Times Square
+        </a>
+      </div>
+    );
+  }
+
+  return (
+    <div className="App">
+      <Game />
+      <GameChanger />
+      <List />
+    </div>
+  );
+}
+
+export default App;
